e0ee94e0684780c524006ce676158ac705fb566c
Siwitisoftware <siwitisoftware@gmail.com>
2026-08-09 13:50:59 +0300

    Update cache version in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='siwiti-enterprise-v2';
+const CACHE='siwiti-enterprise-v3';
 const OFFLINE='/offline.html';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/',OFFLINE]))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
